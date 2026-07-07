cf5e93d3232d4ce426db7811d15d2c8ef5931281
hussainbinfazal <mdhussain0610@gmail.com>
2026-07-07 23:56:44 +0530

    Component initialized

---
+++ day2/src/Component/18Laraval.tsx
@@ -0,0 +1,3 @@
+export async function Laravel(){
+    return(<main className="flex flex-col items-start justify-center min-h-screen bg-linear-to-b from-blue-900 to-blue-800 max-h-screen">Laravel</main>)
+}
